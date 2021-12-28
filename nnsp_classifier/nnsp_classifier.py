c51687acdf5e9b8a031f41eb2294adef89653631
Carlos Arevalo <46701163+caeareva@users.noreply.github.com>
2021-12-27 19:45:01 -0500

    Add files via upload

---
+++ nnsp_classifier/nnsp_classifier.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+#####################################################################################################
+#
+#   Author: Carlos Arevalo (caeareva)
+#
+#   Program execution:
+#   python /Users/carevalo/Desktop/nnsp_classifier/nnsp_classifier.py
+#   
+#   Description:
+#   Data to run program has been provided by by Harrison Kinsley and Daniel Kukieła, authors
+#   of Neural Networks from Scratch in Python (NNSP). If data is inputed as a file, an argparse
+#   module needs to be included in the program (in this file).
+#
+#####################################################################################################
+
+### Neral Networks from Scratch in Python by page 407
+
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+import matplotlib.pyplot as plt
+import layer_dense # Layer_Dense()
+import activation_relu # Activation_ReLU()
+import activation_sigmoid # Activation_Sigmoid
+#import loss_common
+import loss_binary_crossEntropy # Loss_BinaryCrossentropy()
+import optimizer_Adam # Optimizer_Adam()
+
+nnfs.init()
+
+# Create dataset
+X, y = spiral_data(samples=100, classes=2)
+
+# Reshape labels to be a list of lists
+# Inner list contains one output (either 0 or 1)
+# per each output neuron, 1 in this case
+y = y.reshape(-1, 1)
+
+# Create Dense layer with 2 input features and 64 output values 
+dense1 = Layer_Dense(2, 64, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
+
+# Create ReLU activation (to be used with Dense layer):
+
+activation1 = Activation_ReLU()
+
+# Create second Dense layer with 64 input features (as we take output
+# of previous layer here) and 1 output value
+dense2 = Layer_Dense(64, 1)
+
+# Create Sigmoid activation:
+activation2 = Activation_Sigmoid()
+
+# Create loss function
+loss_function = Loss_BinaryCrossentropy()
+
+# Create optimizer
+optimizer = Optimizer_Adam(decay=5e-7) 
+
+# Train in loop
+for epoch in range(10001):
+    # Perform a forward pass of our training data through this layer
+    dense1.forward(X)
+    
+    # Perform a forward pass through activation function
+    # takes the output of first dense layer here
+    activation1.forward(dense1.output)
+    
+    # Perform a forward pass through second Dense layer
+    # takes outputs of activation function
+    # of first layer as inputs
+    dense2.forward(activation1.output)
+    
+    # Perform a forward pass through activation function
+    # takes the output of second dense layer here
+    activation2.forward(dense2.output)
+    
+    # Calculate the data loss
+    data_loss = loss_function.calculate(activation2.output, y)
+    
+    # Calculate regularization penalty
+    regularization_loss = \
+        loss_function.regularization_loss(dense1) + \
+        loss_function.regularization_loss(dense2)
+    
+    # Calculate overall loss
+    loss = data_loss + regularization_loss
+    
+    # Calculate accuracy from output of activation2 and targets
+    # Part in the brackets returns a binary mask - array consisting
+    # of True/False values, multiplying it by 1 changes it into array
+    # of 1s and 0s
+    predictions = (activation2.output > 0.5) * 1
+    accuracy = np.mean(predictions == y)
+    
+    if not epoch % 100: 
+        print(f'epoch: {epoch}, ' +
+              f'acc: {accuracy:.3f}, ' +
+              f'loss: {loss:.3f} (' +
+              f'data_loss: {data_loss:.3f}, ' +
+              f'reg_loss: {regularization_loss:.3f}), ' +
+              f'lr: {optimizer.current_learning_rate}')
+        
+    # Backward pass
+    loss_function.backward(activation2.output, y)
+    activation2.backward(loss_function.dinputs)
+    dense2.backward(activation2.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
+    
+    # Update weights and biases
+    optimizer.pre_update_params()
+    optimizer.update_params(dense1)
+    optimizer.update_params(dense2)
+    optimizer.post_update_params()
+
+# Validate the model
+
+# Create test dataset
+X_test, y_test = spiral_data(samples=100, classes=2)
+
+# Reshape labels to be a list of lists
+# Inner list contains one output (either 0 or 1)
+# per each output neuron, 1 in this case
+y_test = y_test.reshape(-1, 1)
+
+# Perform a forward pass of our testing data through this layer
+dense1.forward(X_test)
+
+# Perform a forward pass through activation function
+# takes the output of first dense layer here
+activation1.forward(dense1.output)
+
+# Perform a forward pass through second Dense layer
+# takes outputs of activation function of first layer as inputs
+dense2.forward(activation1.output)
+
+# Perform a forward pass through activation function
+# takes the output of second dense layer here
+activation2.forward(dense2.output)
+
+# Calculate the data loss
+loss = loss_function.calculate(activation2.output, y_test)
+
+# Calculate accuracy from output of activation2 and targets
+# Part in the brackets returns a binary mask - array consisting of
+# True/False values, multiplying it by 1 changes it into array
+# of 1s and 0s
+predictions = (activation2.output > 0.5)*1
+accuracy = np.mean(predictions == y_test)
+
+print(f'validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
+
